50ea1b0b9bcfc6ad77c2f80beb6c75329f070c3c
v02202 <v0220225@gmail.com>
2020-10-15 11:36:38 +0800

    little modification

--- src/constant/uploadAccept.js
+++ src/constant/uploadAccept.js
@@ -2,7 +2,7 @@ export const acceptDef = {
   image: 'image/jpeg,image/pjpeg,image/png',
   csv: 'text/csv,text/plain',
   excel:
-    'application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
   zip: 'application/zip,application/x-zip-compressed',
   video:
     'video/mp4,application/x-troff-msvideo,video/avi,video/msvideo,video/x-msvideo,video/quicktime,video/mpeg',
